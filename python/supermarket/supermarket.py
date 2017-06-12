2c83a13fb9aec115ad8c7e7ada874e70f91aa7b7
crisoti <cristian.gaspar@gmail.com>
2017-06-12 18:14:45 -0400

    Changed some vars to be instance variables

--- python/supermarket/supermarket.py
+++ python/supermarket/supermarket.py
@@ -1,9 +1,5 @@
 class Product:
-    price = 0.0
-    id = 0
-    name = ""
-
-    def __init__(self, price = 0.0, id = id, name = name):
+    def __init__(self, price=0.0, id=None, name=None):
         self.price = price
         self.id = id
         self.name = name
@@ -13,35 +9,34 @@ class Product:
 
 
 class Discount:
-    discounts = []
-
-    def __init__(self, discounts):
+    def __init__(self, discounts=[]):
         self.discounts = discounts
-        self.discounts.sort(reverse = True)
-        print(self.discounts)
+        self.discounts.sort(reverse=True)
+        print self.discounts
 
 
-    def add(self, count = 0, total_price = 0.0):
+    def add(self, count=0, total_price=0.0):
         if count == 0:
             return
         self.discounts.append((count,total_price))
 
-p1 = Product(id=5019, name="Can of beans", price = 0.65)
-p2 = Product(id=30, name="Bag of socks", price = 9.99)
+p1 = Product(id=5019, name="Can of beans", price=0.65)
+p2 = Product(id=30, name="Bag of socks", price=9.99)
 d1 = [(3,1.0), (5, 1.5), (10, 2.5)]
-d3For1 = Discount(discounts = d1)
+d3For1 = Discount(discounts=d1)
 
 class Checkout:
-    id_to_counts = {}
-    id_to_product = { p1.id: p1, p2.id: p2}
-    id_to_discounts = { p1.id: d3For1}
+    def __init__(self):
+        self.id_to_counts = {}
+        self.id_to_product = {p1.id: p1, p2.id: p2}
+        self.id_to_discounts = {p1.id: d3For1}
 
-    def add(self, id = 0, count = 1):
+    def add(self, id=0, count=1):
         cur_count = self.id_to_counts.get(id, 0)
         self.id_to_counts[id] = cur_count + count
 
     def _calculate_product_price(self, count, product):
-        d = self.id_to_discounts.get(product.id, Discount([]))
+        d = self.id_to_discounts.get(product.id, Discount())
         total = 0
 
         for cnt,discount_price in d.discounts:
@@ -59,12 +54,12 @@ class Checkout:
 
     def get_total(self):
         total = 0.0
-        for id,count in self.id_to_counts.iteritems():
+        for id,num in self.id_to_counts.iteritems():
             p = self.id_to_product[id]
-            total += self._calculate_product_price(count = count, product = p)
+            total += self._calculate_product_price(count=num, product=p)
         return total
 
 
 if __name__ == "__main":
-    p = Product(price = 1.99)
-    print(p.get_price())
+    p = Product(price=1.99)
+    print p.get_price()
