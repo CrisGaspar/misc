b5e16e2c39f798c8b8d9728f6b96e9056779fc45
Cristian Gaspar <cristian.gaspar@gmail.com>
2018-11-14 21:29:00 -0500

    Fixed login and dashboard sidebar menu

--- R/app.R
+++ R/app.R
@@ -13,7 +13,6 @@ library(shinydashboard)
 
 # TODO: 
 # -2: Fix missing 'per Capita' coluumn
-# -1. Make sure the superuser and regular user UI branches are the same for their common elements
 # 0. Login HTTP POST should have hashed password
 # 1. Fix columns like Year + Metric e.g.: 2011 Population etc
 # 2. Fix length of columns to be the same for both data and summary tables.
@@ -34,10 +33,18 @@ source("bma_utils.R", local=TRUE)
 #
 # UI definition
 #
-ui = dashboardPage(
-  dashboardHeader(title = "Login Page"),
-  dashboardSidebar(disable = F),
-  dashboardBody(uiOutput("ui"))
+ui <- dashboardPage(
+  dashboardHeader(title = "BMA Municipal Study"),
+  dashboardSidebar(uiOutput("sidebarpanel")),
+  dashboardBody(uiOutput("body"))
+)
+
+login <- box(
+  title = "Login",
+  textInput("userName", "Username"),
+  passwordInput("passwd", "Password"),
+  br(),
+  actionButton("Login", "Log in")
 )
 
 #
@@ -45,23 +52,135 @@ ui = dashboardPage(
 #
 server <- function(input, output, session) {
   municipality_choices <- reactiveValues(all = list(), selected = list())
-
-  #### UI code --------------------------------------------------------------
-  output$ui <- renderUI({
+  
+  # To logout back to login page
+  login.page = paste(
+    isolate(session$clientData$url_protocol),
+    "//",
+    isolate(session$clientData$url_hostname),
+    ":",
+    isolate(session$clientData$url_port),
+    sep = ""
+  )
+  
+  # reactive value containing user's authentication status
+  user_input <- reactiveValues(authenticated = FALSE, error_message = NULL, is_superuser = F)
+  
+  observe({
     if (user_input$authenticated == FALSE) {
-      ##### UI code for login page
-          column(width = 2, offset = 5,
-                 br(), br(), br(), br(),
-                 uiOutput("uiLogin"),
-                 uiOutput("pass")
-          )
-    } 
-    else if (user_input$is_superuser) {
-      #### App's UI code goes here!
-      dashboardPage(
-        dashboardHeader(title = "Basic dashboard"),
-        dashboardSidebar(
-            collapsed = FALSE,
+      if (!is.null(input$Login)) {
+        if (input$Login > 0) {
+          Username <- isolate(input$userName)
+          Password <- isolate(input$passwd)
+          
+          login_result <- call_login_endpoint(Username, Password)
+          
+          if (login_result$success == "true") {
+            user_input$authenticated <- TRUE
+            user_input$is_superuser <- (login_result$is_superuser == 'TRUE')
+            
+            # Get full list of Municipalities
+            result <- call_API_all_municipalities_endpoint(method = httr::GET)
+            
+            if (result$success == 'false') {
+              showModal(modalDialog(
+                title = "Failed to get full list of municipalities",
+                result$error_message,
+                easyClose = TRUE,
+                footer = NULL))
+              return
+            }
+            
+            municipality_choices$all <- result$municipalities
+            
+            # Get municipalities subset preferred by current user
+            result <- call_API_municipalities_endpoint(method = httr::GET)
+            
+            if (result$success == 'false') {
+              showModal(modalDialog(
+                title = "Failed to get your preferred list of municipalities",
+                result$error_message,
+                easyClose = TRUE,
+                footer = NULL))
+              return
+            }
+            
+            municipality_choices$selected <- result$municipalities
+            user_input$error_message <- NULL
+          } 
+          else {
+            user_input$authenticated <- FALSE
+            user_input$error_message <- login_result$error_message
+            
+            if (!is.null(user_input$error_message)) {
+              h5(strong(paste0("Failed to login. Incorrect credentials.\n"), style = "color:red"), align = "center")
+            } 
+            else {
+              ""
+            }            
+          }
+        }
+      }
+    }
+  })
+
+  
+  #### UI code --------------------------------------------------------------
+  output$sidebarpanel <- renderUI({
+    if (user_input$authenticated == TRUE) {
+      if (user_input$is_superuser) {
+        div(
+          sidebarUserPanel(
+            isolate(input$userName),
+            subtitle = a(icon("usr"), "Logout", href = login.page)
+          ),
+          selectInput(inputId = "municipalitySelector",
+                      label="Custom Grouping",
+                      choices = municipality_choices$all,
+                      selected = municipality_choices$all,
+                      multiple = TRUE,
+                      selectize = FALSE,
+                      size = 10),
+          selectInput(inputId = "data_display_year_selector",
+                      label="Year",
+                      choices = years_all_options,
+                      selected = default_selected_year,
+                      selectize = TRUE),
+          # Horizontal line ----
+          tags$hr(),
+          selectInput(inputId = "data_load_year_selector",
+                        label="Year for Data Import",
+                        choices = years_all_options,
+                        selected = default_selected_year,
+                        selectize = TRUE),
+          
+            # Input: Select a file ----
+            fileInput(inputId = "load_file", "Choose Excel File",
+                      multiple = FALSE,
+                      accept = c("text/xls","text/xlsx")),
+          
+          # Generate the navigation menu
+          # Create menu tab i it's subtabs use menu_sub_tabs_text[[i]] for titles
+          do.call(sidebarMenu, c(id='sidebar_menu', lapply(names(menu_sub_tabs_text), function(tab_name) {
+            do.call(menuItem, c(tab_name, tabName=tab_name, lapply(1:length(menu_sub_tabs_text[[tab_name]]), function(j) {
+              if ((j == 1) && (tab_name == 'Socio Economic Indicators')) {
+                # this is the Population sub-tab in Socio Economic Indicators tab. Set this as the initial selection
+                select = TRUE
+              }
+              else {
+                select = FALSE
+              }
+              menuSubItem(menu_sub_tabs_text[[tab_name]][j], tabName=menu_sub_tabs_text[[tab_name]][j], selected = select)
+            })))
+          })))
+        )
+      }
+      else {
+          div(
+            sidebarUserPanel(
+              isolate(input$userName),
+              subtitle = a(icon("usr"), "Logout", href = login.page)
+            ),
             selectInput(inputId = "municipalitySelector",
                         label="Custom Grouping",
                         choices = municipality_choices$all,
@@ -69,27 +188,13 @@ server <- function(input, output, session) {
                         multiple = TRUE,
                         selectize = FALSE,
                         size = 10),
-            
             selectInput(inputId = "data_display_year_selector",
                         label="Year",
                         choices = years_all_options,
                         selected = default_selected_year,
                         selectize = TRUE),
-
             # Horizontal line ----
             tags$hr(),
-            
-            selectInput(inputId = "data_load_year_selector",
-                        label="Year for Data Import",
-                        choices = years_all_options,
-                        selected = default_selected_year,
-                        selectize = TRUE),
-
-            # Input: Select a file ----
-            fileInput(inputId = "load_file", "Choose Excel File",
-                      multiple = FALSE,
-                      accept = c("text/xls","text/xlsx")),
-            
             # Generate the navigation menu
             # Create menu tab i it's subtabs use menu_sub_tabs_text[[i]] for titles
             do.call(sidebarMenu, c(id='sidebar_menu', lapply(names(menu_sub_tabs_text), function(tab_name) {
@@ -104,75 +209,39 @@ server <- function(input, output, session) {
                 menuSubItem(menu_sub_tabs_text[[tab_name]][j], tabName=menu_sub_tabs_text[[tab_name]][j], selected = select)
               })))
             })))
-        ),
-        dashboardBody(
-          fluidRow(
-            box(
-              downloadButton("downloadData", "Download"),
-              DTOutput("data"),
-              DTOutput("data_stats")
-              # actionButton(inputId="saveUserSelectionButton", label ="Save"),
-              #  actionButton(inputId = "save", label = "Save"),
-              
-              # Output: Tabset w/ plot, summary, and table ----
-              #          tabsetPanel(type = "tabs",
-              #                      tabPanel("Data",
-              #                        DTOutput("data"),
-              #                        DTOutput("data_stats")
-              # Button
-              #downloadButton(export_filename, "Download"),
-              #                      ),
-              #                      tabPanel("Summary", verbatimTextOutput("summary"))
-              #          )
-            )
           )
-        )
-      )
+      }
     }
-    else {
-      #### App's UI code goes here!
-      #### Not superuser
-      fluidPage(
-        selectInput(inputId = "municipalitySelector",
-                    label="Custom Grouping",
-                    choices = municipality_choices$all,
-                    selected = municipality_choices$selected,
-                    multiple = TRUE,
-                    selectize = FALSE,
-                    size = 10),
-        
-        selectInput(inputId = "data_display_year_selector",
-                    label="Select Year for Data Display",
-                    choices = years_all_options,
-                    selected = default_selected_year,
-                    selectize = TRUE),
-        
-        tags$hr(),
-        DTOutput("data"),
-        DTOutput("data_stats"),
-        downloadButton(export_filename, "Download"),
-        
-        # Main panel for displaying outputs ----
-        mainPanel(
-          do.call(navbarPage, c(title = "Data Sets", id='navbar_page', lapply(names(menu_sub_tabs_text), function(sub_tab_name) {
-            do.call(navbarMenu, c(title = sub_tab_name, lapply(1:length(menu_sub_tabs_text[[sub_tab_name]]), function(j) {
-              tabPanel(menu_sub_tabs_text[[sub_tab_name]][j], menu_sub_tabs_text[[sub_tab_name]][j])
-            })))
-          })))
+  })
+  output$body <- renderUI({
+    if (user_input$authenticated == TRUE) {    
+      fluidRow(
+        box(
+          downloadButton("downloadData", "Download"),
+          DTOutput("data"),
+          DTOutput("data_stats")
+          # actionButton(inputId="saveUserSelectionButton", label ="Save"),
+          #  actionButton(inputId = "save", label = "Save"),
+          
+          # Output: Tabset w/ plot, summary, and table ----
+          #          tabsetPanel(type = "tabs",
+          #                      tabPanel("Data",
+          #                        DTOutput("data"),
+          #                        DTOutput("data_stats")
+          # Button
+          #downloadButton(export_filename, "Download"),
+          #                      ),
+          #                      tabPanel("Summary", verbatimTextOutput("summary"))
+          #          )
         )
       )
-    }    
+    } 
+    else {
+      login
+    }
   })
 
   ################### APP SERVER CODE #####################################################
-
-  # TODO: GRAPHS!!!!
-
-  # Generate a summary of the data ----
-  output$summary <- renderPrint({
-    "Summary Test..."
-  })
-
 
   #
   # UI event handling
@@ -307,71 +376,6 @@ server <- function(input, output, session) {
       write_xlsx(merged_df, file)
     }
   )
-
-  #### PASSWORD server code ----------------------------------------------------
-  # reactive value containing user's authentication status
-  user_input <- reactiveValues(authenticated = FALSE, error_message = NULL, is_superuser = F)
-
-  observeEvent(input$login_button, {
-    login_result <- call_login_endpoint(input$user_name, input$password)
-
-    if (login_result$success == "true") {
-      user_input$authenticated <- TRUE
-      user_input$is_superuser <- (login_result$is_superuser == 'TRUE')
-
-      # Get full list of Municipalities
-      result <- call_API_all_municipalities_endpoint(method = httr::GET)
-
-      if (result$success == 'false') {
-        showModal(modalDialog(
-          title = "Failed to get full list of municipalities",
-          result$error_message,
-          easyClose = TRUE,
-          footer = NULL))
-        return
-      }
-    
-      municipality_choices$all <- result$municipalities
-
-      # Get municipalities subset preferred by current user
-      result <- call_API_municipalities_endpoint(method = httr::GET)
-      
-      if (result$success == 'false') {
-        showModal(modalDialog(
-          title = "Failed to get your preferred list of municipalities",
-          result$error_message,
-          easyClose = TRUE,
-          footer = NULL))
-        return
-      }
-      
-      municipality_choices$selected <- result$municipalities
-      user_input$error_message <- NULL
-    } 
-    else {
-      user_input$authenticated <- FALSE
-      user_input$error_message <- login_result$error_message
-    }
-  })
-
-  # password entry UI componenets:
-  #   username and password text fields, login button
-  output$uiLogin <- renderUI({
-    wellPanel(
-      textInput("user_name", "User Name:"),
-      passwordInput("password", "Password:"),
-      actionButton("login_button", "Log in")
-    )
-  })
-
-  # error message if bad credentials
-  output$pass <- renderUI({
-    if (!is.null(user_input$error_message)) {
-      h5(strong(paste0("Failed to login. Incorrect credentials.\n"), style = "color:red"), align = "center")
-    } else {
-      ""
-    }
-  })
 }
 
 # start our web app
