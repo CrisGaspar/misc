bf0fc7350561f28993458258313c160c1c9b3a7a
Cristian Gaspar <cristian.gaspar@gmail.com>
2020-01-09 19:54:57 -0500

    improved new-group-loading script

--- python/bma/load_new_groups.py
+++ python/bma/load_new_groups.py
@@ -1,6 +1,9 @@
 import pandas
 from bmaapp.models import MunicipalityGroup
 
+def delete_groups(data_year):
+    MunicipalityGroup.objects.filter(year=data_year).delete()
+
 def create_groups(file, data_year):
     df = pandas.read_csv(file, header = None)
     for index, row in df.iterrows():
@@ -12,6 +15,7 @@ def create_groups(file, data_year):
         group.municipality_name = municipality_name
         group.save()
 
+delete_groups(2019)
 create_groups("2019_loc_groups.csv", 2019)
 create_groups("2019_pop_groups.csv", 2019)
 create_groups("2019_tier_groups.csv", 2019)
