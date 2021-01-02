11390aff2478a998bd0f38d487fdb8b3e5842c43
Cristian Gaspar <cristian.gaspar@gmail.com>
2021-01-02 15:18:43 -0500

    Updated logins to be a django management command for ease of use

---
+++ python/bma/bmaapp/management/commands/logins.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+import pandas as pd
+
+
+class Command(BaseCommand):
+    help = 'Adds users to django from csv file plus 1 superuser'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csvfile')
+        parser.add_argument('username')
+        parser.add_argument('password')
+
+    # Creates users in DB based on given csv file
+    def create_users(self, file, superuser_name, superuser_password):
+        # read user credentials from given csv file
+        users_data = pd.read_csv(file, header = None)
+
+        # existing db creds: delete instead of upsert snce some users credentials may get revoked hence not present in the given file
+        User.objects.all().delete()
+
+        for index, row in users_data.iterrows():
+            # usernames and passwords are 2nd and 3rd columns respectively
+            current_username = row[1]
+            current_password = row[2]
+            user = User.objects.create_user(username=current_username,password=current_password)
+
+        # create superuser
+        if superuser_name is None or superuser_password is None:
+            self.stdout.write('Missing superuser name or password')
+        else:
+            user = User.objects.create_user(username=superuser_name,password=superuser_password)
+            user.is_superuser=True
+            user.is_staff=True
+            user.save()
+
+    def handle(self, *args, **options):
+        self.create_users(options['csvfile'], options['username'], options['password'])
+
