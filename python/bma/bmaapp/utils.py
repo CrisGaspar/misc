9f581401b4d01947c6dd4310d41da71f7db2effe
Cristian Gaspar <cristian.gaspar@gmail.com>
2021-01-03 17:12:16 -0500

    Fixed extra inner whitespace or lack-of-whitespace-around-dashes in municipality names

--- python/bma/bmaapp/utils.py
+++ python/bma/bmaapp/utils.py
@@ -10,29 +10,34 @@ def convert_db_data(db_data):
     return data
 
 
-name_types = ['Region', 'County']
+name_types = ['Region', 'County', 'District']
 
 # Normalizes region and county names to '<name> Region' or '<name> County'
 def normalize_name(name):
     # Remove redundant and trailing whitespace
     cleaned_name = re.sub(' +', ' ', name.strip())
+    # Ensure that dashes are flanked by exactly 1 space
+    cleaned_parts = cleaned_name.split('-')
+    cleaned_name = ' - '.join([part.strip() for part in cleaned_parts])
+
+    # Split by whitespace so we can do the name types checks
     name_parts = cleaned_name.split()
 
     if len(name_parts) == 1:
         # no match for special types
-        return name
+        return cleaned_name
 
     for name_type in name_types:
-        # check if name starts with 'Region' or 'County'
+        # check if name starts with '<name_type>'
         if name_parts[0].lower() == name_type.lower():
             actual_name_start = 1
-            # check if name starts with 'Region of' or 'County of'
+            # check if name starts with '<name_type> of'
             if name_parts[1].lower() == 'of'.lower():
                 actual_name_start = 2
-            # Re-order so Region or County is at the end
+            # Re-order so <name_type> is at the end
             new_name_parts = name_parts[actual_name_start:]
             new_name_parts.append(name_type)
             return ' '.join(new_name_parts)
 
     # no match for special types
-    return name
+    return cleaned_name
--- python/bma/dataForYearAndMunicipality.py
+++
@@ -1,6 +0,0 @@
-def get_data_for_year(data_year, municipality):
-    dataset_for_year = MunicipalityData.objects.filter(year=data_year).filter(name=municipality)
-    for entry in dataset_for_year:
-        print(entry.est_avg_household_income)
-
-get_data_for_year(2017, "Aurora")
