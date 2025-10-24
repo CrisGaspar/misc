832c7185b81611dda1c18a02788c8f27e49a5a47
Cris Gaspar <cristian.gaspar@gmail.com>
2025-10-24 12:41:55 -0400

    fix: re-enable CSRF protection

--- python/bma/bma/settings.py
+++ python/bma/bma/settings.py
@@ -45,8 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-# Re-enable CSRF!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
